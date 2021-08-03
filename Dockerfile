26f2a545e6ae174b3dc925a0efc3fd5f33bbd5b6
pudge <cgrvel@gmail.com>
2021-08-03 19:26:17 +0530

    updated flask after this req, production deployment

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,7 @@
 FROM python:3.7-stretch
 WORKDIR /code
-ENV FLASK_APP=app.py
-ENV FLASK_RUN_HOST=0.0.0.0
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 EXPOSE 5000
 COPY . .
-CMD ["flask", "run"]
+CMD ["python","app.py"]
--- app.py
+++ app.py
@@ -1,40 +1,48 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
-from flask import Flask, request, abort, send_file, send_from_directory
+from flask import Flask, request, abort, send_file, send_from_directory,after_this_request
 from flask_cors import CORS, cross_origin
+from waitress import serve
 import os
+import logging
 
 from oiAnalyze import analyze_stock
+
 
   
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
 app = Flask(__name__)
+logging.basicConfig(filename='server.log',
+level=logging.DEBUG,
+format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 # The route() function of the Flask class is a decorator, 
 # which tells the application which URL should call 
 # the associated function.
 
-file_to_delete=''
-
 @app.route('/download/<path:index>', methods=['POST'])
 def download(index):
-    global file_to_delete
     analyze_stock(index,request.json)
     file_path = os.path.join(app.root_path, 'data', f'{index}.xlsx')
-    file_to_delete =file_path
-    return send_from_directory(os.path.dirname(file_path),file_path,f'{index}.xlsx')
+    app.logger.info(f'{index} file is saved in {file_path}')
+    @after_this_request
+    def remove_file(res):
+        if os.path.isfile(file_path):
+            app.logger.info(f'deleting file {file_path}')
+            os.remove(file_path)
+        return res
+    print(os.path.dirname(file_path),file_path)
+    return send_from_directory(os.path.dirname(file_path),file_path, f'{index}.xlsx')
 
-@app.after_request
-def remove_file(res):
-    if os.path.isfile(file_to_delete):
-        os.remove(file_to_delete)
-    return res
+
   
 # main driver function
 if __name__ == '__main__':
   
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run(port=1235)
+    # app.run(port=1235)
+    port = 5000
+    serve(app, host='0.0.0.0', port=port)
